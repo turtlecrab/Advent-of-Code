ad5c9284cdb846e651a736ef00e50051ab3b1fbc
Turtle Crab <sk.and.sk@gmail.com>
2022-12-16 22:54:47 +0300

    add day 15.2

--- src/day15.test.ts
+++ src/day15.test.ts
@@ -1,4 +1,10 @@
-import { getScannedPositionsOnLine, parseSensors } from './day15'
+import {
+  findAdjacentSensors,
+  findLineBetween,
+  getLineIntersection,
+  getScannedPositionsOnLine,
+  parseSensors,
+} from './day15'
 
 const testInput = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
@@ -30,5 +36,40 @@ describe('getScannedPositionsOnLine', () => {
     expect(getScannedPositionsOnLine(10, parseSensors(testInput)).size).toBe(26)
     expect(getScannedPositionsOnLine(9, parseSensors(testInput)).size).toBe(25)
     expect(getScannedPositionsOnLine(11, parseSensors(testInput)).size).toBe(27)
+  })
+})
+
+describe('findAdjacentSensors', () => {
+  it('finds all pairs', () => {
+    const adjacent = findAdjacentSensors(parseSensors(testInput))
+    // console.log(adjacent.map(p => [p[0].pos, p[1].pos]))
+    expect(adjacent).toHaveLength(7)
+  })
+})
+
+describe('findLineBetween', () => {
+  const adjacent = findAdjacentSensors(parseSensors(testInput))
+  const pair = adjacent[0]
+  const line = findLineBetween(...pair)
+
+  it('gets the line direction', () => {
+    expect(line.direction).toBe('downright')
+  })
+  it('gets the line position', () => {
+    expect(line.pos.x - line.pos.y).toBe(3)
+  })
+})
+
+describe('get that damn point', () => {
+  it('gets it', () => {
+    const adjacent = findAdjacentSensors(parseSensors(testInput))
+    const lines = adjacent.map(pair => findLineBetween(...pair))
+
+    // This works for test case just out of luck.
+    // And it works for the real input because there are
+    // only 2 adjacent pairs of sensors.
+    // We should check all combinations of crossing lines
+    // and then check every point for being inside another sensor.
+    expect(getLineIntersection(lines[0], lines[1])).toEqual({ x: 14, y: 11 })
   })
 })
--- src/day15.ts
+++ src/day15.ts
@@ -14,6 +14,11 @@ type Sensor = {
   closest: Vec
 }
 
+type Line = {
+  pos: Vec
+  direction: 'upright' | 'downright' // positive y values go down
+}
+
 export function parseSensors(str: string): Sensor[] {
   return str.split('\n').map(line => {
     const [, sx, sy, bx, by] = line.match(
@@ -30,12 +35,16 @@ export function getDistance(a: Vec, b: Vec): number {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
 }
 
+export function getSensorRadius(sensor: Sensor): number {
+  return getDistance(sensor.pos, sensor.closest)
+}
+
 export function getIntersection(
   sensor: Sensor,
   lineNum: number
 ): Set<number> | null {
   const yDist = Math.abs(lineNum - sensor.pos.y)
-  const sensorRadius = getDistance(sensor.pos, sensor.closest)
+  const sensorRadius = getSensorRadius(sensor)
 
   if (yDist > sensorRadius) {
     // no intersection
@@ -78,4 +87,99 @@ export function getScannedPositionsOnLine(
   return result
 }
 
+/**
+ * @returns array of sensor pairs that have exactly 1 pixel space between
+ */
+export function findAdjacentSensors(sensors: Sensor[]): [Sensor, Sensor][] {
+  const result: [Sensor, Sensor][] = []
+  for (let i = 0; i < sensors.length - 1; i++) {
+    for (let j = i + 1; j < sensors.length; j++) {
+      const r1 = getSensorRadius(sensors[i])
+      const r2 = getSensorRadius(sensors[j])
+      const dist = getDistance(sensors[i].pos, sensors[j].pos)
+      if (dist === r1 + r2 + 2) {
+        result.push([sensors[i], sensors[j]])
+      }
+    }
+  }
+  return result
+}
+
+/**
+ * @returns line between 2 adjacent sensors
+ */
+export function findLineBetween(s1: Sensor, s2: Sensor): Line {
+  const pos: Vec = { x: 0, y: 0 }
+  let direction: Line['direction']
+
+  let left: Sensor
+  let right: Sensor
+
+  // TODO: this probably could be implemented more elegantly
+  if (s1.pos.x > s2.pos.x) {
+    left = s1
+    right = s2
+  } else {
+    left = s2
+    right = s1
+  }
+  if (left.pos.y > right.pos.y) {
+    // line goes to up right -> //
+    pos.x = left.pos.x
+    pos.y = left.pos.y - getSensorRadius(left) - 1
+    direction = 'upright'
+  } else {
+    // line goes to down right -> \\
+    pos.x = left.pos.x
+    pos.y = left.pos.y + getSensorRadius(left) + 1
+    direction = 'downright'
+  }
+  return {
+    pos,
+    direction,
+  }
+}
+
+/**
+ * @returns intersection point between 2 crossing lines
+ */
+export function getLineIntersection(line1: Line, line2: Line): Vec {
+  if (line1.direction === line2.direction) throw new Error('lines are parallel')
+
+  const downRight = line1.direction === 'downright' ? line1 : line2
+  const upRight = line1.direction === 'upright' ? line1 : line2
+
+  // TODO: this could be implemented more elegantly too
+  // e.g. by offsetting x or y line positions to 0
+  const projectedUpRight: Line = {
+    ...upRight,
+    pos: {
+      x: downRight.pos.x,
+      y: upRight.pos.y + upRight.pos.x - downRight.pos.x,
+    },
+  }
+  const halfDistance = (downRight.pos.y - projectedUpRight.pos.y) / 2
+
+  const x = downRight.pos.x - halfDistance
+  const y = downRight.pos.y - halfDistance
+
+  return { x, y }
+}
+
+// part 1
 console.log(getScannedPositionsOnLine(2000000, parseSensors(input)).size)
+
+// part 2
+const adjacent = findAdjacentSensors(parseSensors(input))
+const lines = adjacent.map(pair => findLineBetween(...pair))
+
+// TODO
+// I think we also need to find all possible intersection points of all
+// combinations of adjacent sensors, and then check them against all
+// sensors to filter the ones that are inside some other sensor.
+
+// But there is only one combination of adjacent pairs in the input and I'm
+// really tired of this puzzle, so I'll leave this comment instead.
+const point = getLineIntersection(lines[0], lines[1])
+
+console.log(point.x * 4_000_000 + point.y)
