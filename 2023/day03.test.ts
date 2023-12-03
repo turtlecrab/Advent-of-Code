1bfb88fa3fe7e4718c5be780d7db84d20809a726
Turtle Crab <sk.and.sk@gmail.com>
2023-12-03 19:02:13 +0300

    add 2023/03.2

--- 2023/day03.test.ts
+++ 2023/day03.test.ts
@@ -1,4 +1,11 @@
-import { getNumbersAdjacentToSymbols, parseMap } from './day03'
+import {
+  filterProperGears,
+  getAdjacentGears,
+  getAllGears,
+  getNumbersAdjacentToSymbols,
+  parseMap,
+  sumOfGearRatios,
+} from './day03'
 
 const testInput = `467..114..
 ...*......
@@ -15,5 +22,25 @@ describe('getNumbersAdjacentToSymbols', () => {
   it('works', () => {
     const nums = getNumbersAdjacentToSymbols(parseMap(testInput))
     expect(nums.reduce((a, c) => a + c)).toBe(4361)
+  })
+})
+
+const map = parseMap(testInput)
+
+describe('getAdjacentGears', () => {
+  it('works', () => {
+    expect(getAdjacentGears(map, 0, 0, 3)).toEqual(['1:3'])
+    expect(getAdjacentGears(map, 3, 0, 3)).toEqual(['4:3'])
+    expect(getAdjacentGears(map, 4, 0, 3)).toEqual(['4:3'])
+    expect(getAdjacentGears(map, 5, 0, 3)).toEqual(['4:3'])
+    expect(getAdjacentGears(map, 7, 6, 9)).toEqual(['8:5'])
+    expect(getAdjacentGears(map, 9, 5, 8)).toEqual(['8:5'])
+    expect(getAdjacentGears(map, 8, 6, 9)).toEqual(['8:5'])
+  })
+})
+
+describe('sumOfGearRatios', () => {
+  it('sums it', () => {
+    expect(sumOfGearRatios(filterProperGears(getAllGears(map)))).toBe(467835)
   })
 })
--- 2023/day03.ts
+++ 2023/day03.ts
@@ -58,6 +58,79 @@ export function getNumbersAdjacentToSymbols(map: Map): number[] {
   })
 }
 
+type Gears = {
+  [key: string]: number[] // "y:x" : [1, 2, ...]
+}
+
+export function getAdjacentGears(
+  map: Map,
+  y: number,
+  startX: number,
+  endX: number
+): string[] {
+  const result: string[] = []
+
+  const isGear = (y: number, x: number) => map[y]?.[x] === '*'
+  const serialize = (y: number, x: number) => `${y}:${x}`
+
+  for (let x = startX - 1; x <= endX; x++) {
+    // above
+    if (isGear(y - 1, x)) result.push(serialize(y - 1, x))
+    // below
+    if (isGear(y + 1, x)) result.push(serialize(y + 1, x))
+  }
+  // left
+  if (isGear(y, startX - 1)) result.push(serialize(y, startX - 1))
+  // right
+  if (isGear(y, endX)) result.push(serialize(y, endX))
+
+  return result
+}
+
+export function getAllGears(map: Map): Gears {
+  const result: Gears = {}
+
+  for (let y = 0; y < map.length; y++) {
+    let numBuffer = ''
+
+    for (let x = 0; x <= map[y].length; x++) {
+      const char = map[y][x]
+
+      if (/\d/.test(char)) {
+        numBuffer += char
+      } else if (numBuffer.length > 0) {
+        const gears = getAdjacentGears(map, y, x - numBuffer.length, x)
+        for (let gear of gears) {
+          if (gear in result) {
+            result[gear].push(Number(numBuffer))
+          } else {
+            result[gear] = [Number(numBuffer)]
+          }
+        }
+        numBuffer = ''
+      }
+    }
+  }
+  return result
+}
+
+export function filterProperGears(gears: Gears): Gears {
+  return Object.keys(gears)
+    .filter(gear => gears[gear].length === 2)
+    .reduce((acc, cur) => {
+      acc[cur] = gears[cur]
+      return acc
+    }, {})
+}
+
+export function sumOfGearRatios(gears: Gears): number {
+  return Object.values(gears)
+    .map(([a, b]) => a * b)
+    .reduce((a, c) => a + c)
+}
+
 console.log(
   getNumbersAdjacentToSymbols(parseMap(input)).reduce((a, c) => a + c)
 )
+
+console.log(sumOfGearRatios(filterProperGears(getAllGears(parseMap(input)))))
