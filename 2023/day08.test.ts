3d33e1ae3c96ca4395c78c7b176b4a38939422c8
Turtle Crab <sk.and.sk@gmail.com>
2023-12-08 18:35:46 +0300

    add 2023/08.2

--- 2023/day08.test.ts
+++ 2023/day08.test.ts
@@ -1,4 +1,10 @@
-import { parseAll, play } from './day08'
+import {
+  getPrimeDividers,
+  getLowestPrimeDivider,
+  parseAll,
+  play,
+  playParallel,
+} from './day08'
 
 const testInput1 = `RL
 
@@ -15,6 +21,17 @@ const testInput2 = `LLR
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`
+
+const testInput3 = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
 
 describe('parseAll', () => {
   it('parses test inputs', () => {
@@ -38,5 +55,36 @@ describe('play', () => {
   })
   it('counts test input 2', () => {
     expect(play(...parseAll(testInput2))).toBe(6)
+  })
+})
+
+describe('getLowestPrimeDivider', () => {
+  it('gets it', () => {
+    expect(getLowestPrimeDivider(1)).toBe(1)
+    expect(getLowestPrimeDivider(2)).toBe(2)
+    expect(getLowestPrimeDivider(3)).toBe(3)
+    expect(getLowestPrimeDivider(4)).toBe(2)
+    expect(getLowestPrimeDivider(5)).toBe(5)
+    expect(getLowestPrimeDivider(6)).toBe(2)
+    expect(getLowestPrimeDivider(7)).toBe(7)
+    expect(getLowestPrimeDivider(8)).toBe(2)
+    expect(getLowestPrimeDivider(9)).toBe(3)
+    expect(getLowestPrimeDivider(997)).toBe(997)
+  })
+})
+
+describe('getPrimeDividers', () => {
+  it('gets em', () => {
+    expect(getPrimeDividers(1)).toEqual([1])
+    expect(getPrimeDividers(2)).toEqual([2])
+    expect(getPrimeDividers(3)).toEqual([3])
+    expect(getPrimeDividers(4)).toEqual([2, 2])
+    expect(getPrimeDividers(12)).toEqual([2, 2, 3])
+  })
+})
+
+describe('playParallel', () => {
+  it('counts test input 3', () => {
+    expect(playParallel(...parseAll(testInput3))).toBe(6)
   })
 })
--- 2023/day08.ts
+++ 2023/day08.ts
@@ -38,4 +38,52 @@ export function play(moves: string, nodes: Nodes): number {
   return count
 }
 
+export function getAllStartingNodes(nodes: Nodes): string[] {
+  return Object.keys(nodes).filter(node => /A$/.test(node))
+}
+
+export function getLowestPrimeDivider(num: number): number {
+  const sqrt = Math.sqrt(num)
+
+  for (let i = 2; i <= sqrt; i++) {
+    const div = num / i
+    if (Math.floor(div) === div) {
+      return i
+    }
+  }
+  return num
+}
+
+export function getPrimeDividers(num: number): number[] {
+  const next = getLowestPrimeDivider(num)
+
+  if (next === num) {
+    return [num]
+  }
+  return [next, ...getPrimeDividers(num / next)]
+}
+
+export function playParallel(moves: string, nodes: Nodes): number {
+  const currents = getAllStartingNodes(nodes)
+  const results: number[] = []
+
+  for (let current of currents) {
+    let count = 0
+
+    while (!/Z$/.test(current)) {
+      const dir = moves[count % moves.length] === 'L' ? 'left' : 'right'
+      current = nodes[current][dir]
+      count += 1
+    }
+    results.push(count)
+  }
+
+  // this is working for the given input, but not for any input.
+  // e.g. lowest common denominator for [8, 16] is 16
+  // but this return expression would give 2
+  return [...new Set(results.flatMap(getPrimeDividers))].reduce((a, c) => a * c)
+}
+
 console.log(play(...parseAll(input)))
+
+console.log(playParallel(...parseAll(input)))
