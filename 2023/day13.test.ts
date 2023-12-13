5402316390b52f2bc00019ac6557ad0c1a0dd992
Turtle Crab <sk.and.sk@gmail.com>
2023-12-13 23:08:44 +0300

    add 2023/13.2

--- 2023/day13.test.ts
+++ 2023/day13.test.ts
@@ -1,4 +1,10 @@
-import { findSymmetry, getSum, parsePatterns } from './day13'
+import {
+  findSmudgedSymmetry,
+  findSymmetry,
+  getDiff,
+  getSum,
+  parsePatterns,
+} from './day13'
 
 const testInput = `#.##..##.
 ..#.##.#.
@@ -17,15 +23,32 @@ const testInput = `#.##..##.
 #....#..#`
 
 describe('findSymmetry', () => {
-  it('does stuff', () => {
+  it('finds it', () => {
     const patterns = parsePatterns(testInput)
     expect(findSymmetry(patterns[0])).toEqual({ type: '|', index: 5 })
     expect(findSymmetry(patterns[1])).toEqual({ type: '-', index: 4 })
   })
 })
 
+describe('getDiff', () => {
+  it('gets amount of different chars', () => {
+    expect(getDiff('.........', '#########')).toBe(9)
+    expect(getDiff('#####.##.', '#####.##.')).toBe(0)
+    expect(getDiff('#.##..##.', '..##..##.')).toBe(1)
+  })
+})
+
+describe('findSmudgedSymmetry', () => {
+  it('finds it', () => {
+    const patterns = parsePatterns(testInput)
+    expect(findSmudgedSymmetry(patterns[0])).toEqual({ type: '-', index: 3 })
+    expect(findSmudgedSymmetry(patterns[1])).toEqual({ type: '-', index: 1 })
+  })
+})
+
 describe('getSum', () => {
   it('gets it', () => {
     expect(getSum(parsePatterns(testInput))).toBe(405)
+    expect(getSum(parsePatterns(testInput), true)).toBe(400)
   })
 })
--- 2023/day13.ts
+++ 2023/day13.ts
@@ -44,11 +44,52 @@ export function findSymmetry(pattern: string[]): {
   throw new Error('no symmetry')
 }
 
-export function getSum(patterns: string[][]): number {
+export function getDiff(a: string, b: string): number {
+  return a
+    .split('')
+    .map((char, i) => char !== b[i])
+    .filter(Boolean).length
+}
+
+export function findSmudgedSymmetry(pattern: string[]): {
+  type: '-' | '|'
+  index: number
+} {
+  // horizontal
+  for (let i = 1; i < pattern.length; i++) {
+    let diff = 0
+
+    for (let j = 0; j < Math.min(pattern.length - i, i); j++) {
+      const up = pattern[i - j - 1]
+      const down = pattern[i + j]
+
+      diff += getDiff(up, down)
+    }
+    if (diff === 1) return { type: '-', index: i }
+  }
+  // vertical
+  const width = pattern[0].length
+  for (let i = 1; i < width; i++) {
+    let diff = 0
+
+    for (let j = 0; j < Math.min(width - i, i); j++) {
+      const left = pattern.map(row => row[i - j - 1]).join('')
+      const right = pattern.map(row => row[i + j]).join('')
+
+      diff += getDiff(left, right)
+    }
+    if (diff === 1) return { type: '|', index: i }
+  }
+  throw new Error('no symmetry')
+}
+
+export function getSum(patterns: string[][], withSmudge = false): number {
   return patterns
-    .map(findSymmetry)
+    .map(withSmudge ? findSmudgedSymmetry : findSymmetry)
     .map(sym => (sym.type === '|' ? sym.index : sym.index * 100))
     .reduce((a, b) => a + b)
 }
 
 console.log(getSum(parsePatterns(input)))
+
+console.log(getSum(parsePatterns(input), true))
