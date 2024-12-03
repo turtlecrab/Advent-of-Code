8e156bb3caabb8d1763cbb556a83a9bbd61a35ee
Turtle Crab <sk.and.sk@gmail.com>
2024-12-03 13:51:32 +0300

    add 2024/03.2

--- 2024/day03.test.ts
+++ 2024/day03.test.ts
@@ -1,9 +1,21 @@
-import { getValidMultiplicationsSum } from './day03'
+import {
+  getValidMultiplicationsSum,
+  parseValidMultiplications,
+  parseValidMultiplications2,
+} from './day03'
 
 const testInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const testInput2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 describe('getValidMultiplicationsSum', () => {
-  it('works for test input', () => {
-    expect(getValidMultiplicationsSum(testInput)).toBe(161)
+  it('works for test input part 1', () => {
+    expect(
+      getValidMultiplicationsSum(parseValidMultiplications(testInput))
+    ).toBe(161)
+  })
+  it('works for test input part 2', () => {
+    expect(
+      getValidMultiplicationsSum(parseValidMultiplications2(testInput2))
+    ).toBe(48)
   })
 })
--- 2024/day03.ts
+++ 2024/day03.ts
@@ -4,12 +4,37 @@ const input = fs
   .readFileSync(__dirname + '/day03.input.txt', 'utf8')
   .replace(/\r/g, '')
 
-export function getValidMultiplications(input: string): number[][] {
+export function parseValidMultiplications(input: string): number[][] {
   return input.match(/mul\(\d+,\d+\)/g).map(s => s.match(/\d+/g).map(Number))
 }
 
-export function getValidMultiplicationsSum(input: string): number {
-  return getValidMultiplications(input).reduce((acc, [a, b]) => acc + a * b, 0)
+export function parseValidMultiplications2(input: string): number[][] {
+  const instructions = input.match(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/g)
+  const result: number[][] = []
+
+  let doNext = true
+
+  for (let instruction of instructions) {
+    switch (true) {
+      case instruction.includes('mul'):
+        if (doNext) {
+          result.push(instruction.match(/\d+/g).map(Number))
+        }
+        break
+      case instruction === "don't()":
+        doNext = false
+        break
+      case instruction === 'do()':
+        doNext = true
+        break
+    }
+  }
+  return result
 }
 
-console.log(getValidMultiplicationsSum(input))
+export function getValidMultiplicationsSum(data: number[][]): number {
+  return data.reduce((acc, [a, b]) => acc + a * b, 0)
+}
+
+console.log(getValidMultiplicationsSum(parseValidMultiplications(input)))
+console.log(getValidMultiplicationsSum(parseValidMultiplications2(input)))
